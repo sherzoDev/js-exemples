47698ad4be9206f8c657f3f64c66c34377683d56
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-09-28 22:21:31 +0500

    Add JavaScript semi columns

--- 1-exemple/main.js
+++ 1-exemple/main.js
@@ -1,15 +1,15 @@
-var user = prompt("Ismingizni kiriting");
-var surname = prompt("Familangizni kiriting");
-var age = prompt("Yoshingizni kiriting");
-var passportid = prompt("Passport Idni kiriting");
-var valut = Number(prompt("Valutani kiriting") * 11040);
+var user = prompt("Ismingizni kiriting:");
+var surname = prompt("Familangizni kiriting:");
+var age = prompt("Yoshingizni kiriting:");
+var passportid = prompt("Passport Idni kiriting:");
+var valut = Number(prompt("Valutani kiriting:") * 11040);
 
-console.log(`Sizning ismingiz ${user}  Sizning familangiz ${surname}
-Sizning yoshingiz ${age}
-Valuta so'mda ${valut}`);
+console.log(`Sizning ismingiz: ${user},  Sizning familangiz: ${surname},
+Sizning yoshingiz ${age},
+AQSH dollari so'mda ${valut}`);
 
 var pass = document.getElementById("pass")
 
-pass.innerHTML=(`Sizning ismingiz ${user} <br>  Sizning familangiz ${surname} <br>
-Sizning yoshingiz ${age} <br>
-Valuta so'mda ${valut}`);
+pass.innerHTML=(`Sizning ismingiz: ${user}, <br>  Sizning familangiz: ${surname}, <br>
+Sizning yoshingiz: ${age}, <br>
+AQSH dollari so'mda: ${valut}`);
