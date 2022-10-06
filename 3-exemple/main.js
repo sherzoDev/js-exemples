a65772a37c31f358ab0c03bd3a5c3f07608e8128
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-10-06 17:02:37 +0500

    Add FizzBuzz DOM

--- 3-exemple/main.js
+++ 3-exemple/main.js
@@ -2,19 +2,19 @@ var elForm = document.querySelector("#form");
 var elInput = document.querySelector(".form-control");
 var elAlert = document.querySelector(".alert");
 
-elForm.addEventListener("submit", function(even) {
+elForm.addEventListener("submit", function (even) {
   even.preventDefault();
   var inputValue = Number(elInput.value);
   if (inputValue % 3 == 0 && inputValue % 5 === 0) {
-  elAlert.textContent = `FizzBuzz`;
-} else if (inputValue % 5 === 0 ) {
-  elAlert.textContent = `Buzz!`;
-} else if (inputValue % 3 === 0 ) {
-  elAlert.textContent = `Fizz`;
-} else {
-  elAlert.textContent = inputValue + " " + "Bu son bo'linmaydi";
-}
+    elAlert.textContent = `FizzBuzz`;
+  } else if (inputValue % 5 === 0) {
+    elAlert.textContent = `Buzz!`;
+  } else if (inputValue % 3 === 0) {
+    elAlert.textContent = `Fizz`;
+  } else {
+    elAlert.textContent = inputValue + " " + "Bu son bo'linmaydi";
+  }
 
-elInput.value = "";
+  elInput.value = "";
 
 })
