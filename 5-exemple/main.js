74c4c30e55125fbc6dbcc60b5748ff79f088cf06
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-10-06 23:24:30 +0500

    Add Converter

---
+++ 5-exemple/main.js
@@ -0,0 +1,10 @@
+var form = document.querySelector(".form");
+var input = document.querySelector(".form-control");
+var select = document.querySelector(".form-select");
+var span = document.querySelector(".span");
+
+form.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    
+    span.textContent = input.value * select.value;
+})
